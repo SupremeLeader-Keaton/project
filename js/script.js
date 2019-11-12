43cab79e59be278a1942c3b9ce361abcd062171c
SupremeLeader-Keaton <keaton.yoputra@gmail.com>
2019-11-12 07:52:20 +0700

    ProDoc+ Major Revamp (Part1)

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-$(document).ready(function(){
-    
-    $('[data-url]').each(function(i, DOM){
-      $(DOM).load($(DOM).data('url'))
- 
-    })
+$(document).ready(function () {
+
+  $('[data-url]').each(function (i, DOM) {
+    $(DOM).load($(DOM).data('url'))
+
+  })
 })
+
+function showName(e){
+  var firstName= document.getElementById("firstname").value;
+  var lastName = document.getElementById("lastname").value;
+  document.getElementById("show").innerText = firstName+" "+lastName;        
+}
